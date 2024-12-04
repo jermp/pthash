ce9bcce46f4f88e2b6111e22853347106e0da472
Giulio Ermanno Pibiri <jeis90@gmail.com>
2024-12-04 14:37:54 +0100

    style; status badge on README

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build](https://github.com/jermp/pthash/actions/workflows/build.yml/badge.svg)](https://github.com/jermp/pthash/actions/workflows/build.yml)
 [![CodeQL](https://github.com/jermp/pthash/actions/workflows/codeql.yml/badge.svg)](https://github.com/jermp/pthash/actions/workflows/codeql.yml)
 
 PTHash
--- include/utils/bucketers.hpp
+++ include/utils/bucketers.hpp
@@ -8,42 +8,42 @@ namespace pthash {
 
 template <typename Bucketer>
 struct table_bucketer {
-    table_bucketer() : base(Bucketer()), fulcrums() {}
+    table_bucketer() : m_base(Bucketer()), m_fulcrums() {}
 
     void init(const uint64_t num_buckets, const double lambda, const uint64_t table_size,
               const double alpha) {
-        base.init(num_buckets, lambda, table_size, alpha);
+        m_base.init(num_buckets, lambda, table_size, alpha);
 
-        fulcrums[0] = 0;
+        m_fulcrums[0] = 0;
         for (size_t xi = 0; xi < FULCS - 1; xi++) {
             double x = double(xi) / double(FULCS - 1);
-            double y = base.bucketRelative(x);
+            double y = m_base.bucketRelative(x);
             auto fulcV = uint64_t(y * double(num_buckets << 16));
-            fulcrums[xi + 1] = fulcV;
+            m_fulcrums[xi + 1] = fulcV;
         }
-        fulcrums[FULCS - 1] = num_buckets << 16;
+        m_fulcrums[FULCS - 1] = num_buckets << 16;
     }
 
     inline uint64_t bucket(const uint64_t hash) const {
         uint64_t z = (hash & 0xFFFFFFFF) * uint64_t(FULCS - 1);
         uint64_t index = z >> 32;
         uint64_t part = z & 0xFFFFFFFF;
-        uint64_t v1 = (fulcrums[index + 0] * part) >> 32;
-        uint64_t v2 = (fulcrums[index + 1] * (0xFFFFFFFF - part)) >> 32;
+        uint64_t v1 = (m_fulcrums[index + 0] * part) >> 32;
+        uint64_t v2 = (m_fulcrums[index + 1] * (0xFFFFFFFF - part)) >> 32;
         return (v1 + v2) >> 16;
     }
 
     uint64_t num_buckets() const {
-        return base.num_buckets();
+        return m_base.num_buckets();
     }
 
     size_t num_bits() const {
-        return base.num_buckets() + fulcrums.size() * 64;
+        return m_base.num_buckets() + m_fulcrums.size() * 64;
     }
 
     void swap(table_bucketer& other) {
-        base.swap(other.base);
-        std::swap(fulcrums, other.fulcrums);
+        m_base.swap(other.m_base);
+        std::swap(m_fulcrums, other.m_fulcrums);
     }
 
     template <typename Visitor>
@@ -59,29 +59,29 @@ struct table_bucketer {
 private:
     template <typename Visitor, typename T>
     static void visit_impl(Visitor& visitor, T&& t) {
-        visitor.visit(t.fulcrums);
-        visitor.visit(t.base);
+        visitor.visit(t.m_fulcrums);
+        visitor.visit(t.m_base);
     }
 
-    Bucketer base;
+    Bucketer m_base;
     static const uint64_t FULCS = 2048;
-    std::array<uint64_t, FULCS> fulcrums;
+    std::array<uint64_t, FULCS> m_fulcrums;
 };
 
 struct opt_bucketer {
-    opt_bucketer() : c(0), m_num_buckets(0), m_alpha(0), m_alpha_factor(0) {}
+    opt_bucketer() : m_c(0), m_num_buckets(0), m_alpha(0), m_alpha_factor(0) {}
 
     inline double baseFunc(const double normalized_hash) const {
         return (normalized_hash + (1 - normalized_hash) * std::log(1 - normalized_hash)) *
-                   (1.0 - c) +
-               c * normalized_hash;
+                   (1.0 - m_c) +
+               m_c * normalized_hash;
     }
 
     void init(const uint64_t num_buckets, const double lambda, const uint64_t table_size,
               const double alpha) {
         m_num_buckets = num_buckets;
         m_alpha = alpha;
-        c = 0.2 * lambda / std::sqrt(table_size);
+        m_c = 0.2 * lambda / std::sqrt(table_size);
         if (alpha > 0.9999) {
             m_alpha_factor = 1.0;
         } else {
@@ -107,12 +107,12 @@ struct opt_bucketer {
     }
 
     size_t num_bits() const {
-        return 8 * sizeof(m_num_buckets) + 8 * sizeof(c) + 8 * sizeof(m_alpha) +
+        return 8 * sizeof(m_num_buckets) + 8 * sizeof(m_c) + 8 * sizeof(m_alpha) +
                8 * sizeof(m_alpha_factor);
     }
 
     void swap(opt_bucketer& other) {
-        std::swap(c, other.c);
+        std::swap(m_c, other.m_c);
         std::swap(m_num_buckets, other.m_num_buckets);
         std::swap(m_alpha, other.m_alpha);
         std::swap(m_alpha_factor, other.m_alpha_factor);
@@ -132,11 +132,12 @@ private:
     template <typename Visitor, typename T>
     static void visit_impl(Visitor& visitor, T&& t) {
         visitor.visit(t.m_num_buckets);
-        visitor.visit(t.c);
+        visitor.visit(t.m_c);
         visitor.visit(t.m_alpha);
         visitor.visit(t.m_alpha_factor);
     }
-    double c;
+
+    double m_c;
     uint64_t m_num_buckets;
     double m_alpha;
     double m_alpha_factor;
@@ -292,6 +293,7 @@ private:
         visitor.visit(t.m_num_buckets);
         visitor.visit(t.m_M_num_buckets);
     }
+
     uint64_t m_num_buckets;
     __uint128_t m_M_num_buckets;
 };
